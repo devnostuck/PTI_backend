b9233d212bca97c53787d5188f32cc00474c6340
postoev.ie <postoev.ie@gmail.com>
2019-07-07 23:10:54 +0300

    Init: servlet returning list of users

---
+++ src/main/java/ControllerServlet.java
@@ -0,0 +1,56 @@
+
+import com.google.gson.Gson;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * ControllerServlet.java
+ * This servlet acts as a page controller for the application, handling all
+ * requests from the user.
+ * @author www.codejava.net
+ */
+public class ControllerServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private userDAO bookDAO;
+
+    public void init() {
+        String jdbcURL = getServletContext().getInitParameter("jdbcURL");
+        String jdbcUsername = getServletContext().getInitParameter("jdbcUsername");
+        String jdbcPassword = getServletContext().getInitParameter("jdbcPassword");
+        System.out.println("initServlet");
+        bookDAO = new userDAO(jdbcURL, jdbcUsername, jdbcPassword);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("getRequest");
+        try {
+            listBook(request, response);
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    private void listBook(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        List<User> listUser = bookDAO.listAllUsers();
+        String usersJsonString = new Gson().toJson(listUser);
+        System.out.println(usersJsonString);
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(usersJsonString);
+        out.flush();
+    }
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,56 @@
+
+public class User {
+    int id;
+    String first_name;
+    String last_name;
+    int age;
+    String gender;
+
+    public User(int id, String first_name, String last_name, int age, String gender) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
