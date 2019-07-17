f50bb83781f625fe4762ebab2d42ef095e036e98
postoev.ie <postoev.ie@gmail.com>
2019-07-17 11:40:06 +0300

    test commit

--- src/main/java/ControllerServlet.java
+++ src/main/java/ControllerServlet.java
@@ -1,54 +1,17 @@
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.ModelMap;
 
-import com.google.gson.Gson;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.List;
-
-/**
- * ControllerServlet.java
- * This servlet acts as a page controller for the application, handling all
- * requests from the user.
- * @author www.codejava.net
- */
-public class ControllerServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    private UserDAO userDAO;
-
-    public void init() {
-        String jdbcURL = getServletContext().getInitParameter("jdbcURL");
-        String jdbcUsername = getServletContext().getInitParameter("jdbcUsername");
-        String jdbcPassword = getServletContext().getInitParameter("jdbcPassword");
-        userDAO = new UserDAO(jdbcURL, jdbcUsername, jdbcPassword);
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doGet(request, response);
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        System.out.println("getRequest");
-        try {
-            listBook(request, response);
-        } catch (SQLException ex) {
-            throw new ServletException(ex);
-        }
-    }
-
-    private void listBook(HttpServletRequest request, HttpServletResponse response)
-            throws SQLException, IOException, ServletException {
-        List<User> listUser = userDAO.listAllUsers();
-        String usersJsonString = new Gson().toJson(listUser);
-        PrintWriter out = response.getWriter();
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        out.print(usersJsonString);
-        out.flush();
+@Controller
+public class ControllerServlet {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String student() {
+        return "Hello";
     }
 }
