160ad32a55fd5866d70f5579ad042fb9a6729b81
postoev.ie <postoev.ie@gmail.com>
2019-07-17 13:03:44 +0300

    added all

---
+++ src/main/java/pti/config/AppInitializer.java
@@ -0,0 +1,20 @@
+package pti.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{HibernateConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
